3ad6171a9a1c74c4005c08d893b5d9422b77e2b4
torvic <130111@unsaac.edu.pe>
2021-07-27 23:39:53 -0500

    Feat(login): Add validation and integration with API login

--- ecommerce-artesania/src/hooks/useForm/index.jsx
+++ ecommerce-artesania/src/hooks/useForm/index.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
+import { useHistory } from "react-router";
 import { helpHttp } from "../../helpers/helpHttp";
 
 export const useForm = (initialForm, validateForm) => {
   const [form, setForm] = useState(initialForm);
   const [errors, setErrors] = useState({});
   const [loading, setLoading] = useState(false);
-  const [response, setResponse] = useState(null);
+  const [auth, setAuth] = useState(null);
 
+	let history = useHistory();
+	
   const handleChange = (e) => {
     const { name, value } = e.target;
     setForm({
@@ -18,6 +21,7 @@ export const useForm = (initialForm, validateForm) => {
   const handleBlur = (e) => {
     handleChange(e);
     setErrors(validateForm(form));
+		setAuth(false)
   };
 
   const handleSubmit = (e) => {
@@ -28,7 +32,7 @@ export const useForm = (initialForm, validateForm) => {
       //alert("Enviando formulario...");
       setLoading(true);
       helpHttp()
-        .post("https://formsubmit.co/ajax/130111@unsaac.edu.pe", {
+        .post("http://127.0.0.1:8000/accounts/api/login/", {
           body: form,
           headers: {
             "Content-Type": "application/json",
@@ -36,12 +40,17 @@ export const useForm = (initialForm, validateForm) => {
           },
         })
         .then((res) => {
+					console.log(res);
+					if (res.err) {
+						setAuth(true)
+						/* setTimeout(() => {
+							setResponse(false);	
+						}, 5000); */
+					} else {
+						history.push('/')
+					}
 					setForm(initialForm);
           setLoading(false);
-          setResponse(true);
-					setTimeout(() => {
-						setResponse(false);	
-					}, 5000);
         });
     } else {
       return;
@@ -52,7 +61,7 @@ export const useForm = (initialForm, validateForm) => {
     form,
     errors,
     loading,
-    response,
+    auth,
     handleChange,
     handleBlur,
     handleSubmit,
--- ecommerce-artesania/src/sections/Public/Login/FormSection/index.jsx
+++ ecommerce-artesania/src/sections/Public/Login/FormSection/index.jsx
@@ -1,37 +1,96 @@
 import styles from './index.module.scss'
 import { useForm } from '../../../../hooks/useForm'
+import Message from '../../../../components/Message'
+import Loader from '../../../../components/Loader'
+
+const initialForm = {
+  username: '',
+  password: '',
+}
 
 const FormSection = () => {
+  const validationsForm = (form) => {
+    let errors = {}
+    let regexName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/
+    let regexPassword =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/
+    // let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/
+    // let regexComments = /^.{1,255}$/
+
+    if (!form.username.trim()) {
+      errors.username = "El campo 'Username' es requerido."
+    } else if (!regexName.test(form.username.trim())) {
+      errors.username =
+        "El campo 'Username' solo acepta letras y espacios en blanco."
+    }
+
+    if (!form.password.trim()) {
+      errors.password = "El campo 'Password' es requerido."
+    } else if (!regexPassword.test(form.password.trim())) {
+      errors.password =
+        "El campo 'Password' acepta de 8 a 15 caracteres, al menos un caracter especial, un digito y no espacios en blanco"
+    }
+
+    return errors
+  }
+
+  const {
+    form,
+    errors,
+    loading,
+    auth,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+  } = useForm(initialForm, validationsForm)
   return (
-    <form>
-      <div className='form-group'>
-        <label for='username'>Username</label>
-        <input
-          type='text'
-					name='username'
-          className='form-control is-invalid'
-          id='username'
-        />
-				{/* <div className="valid-feedback">
-          Looks good!
-        </div> */}
-      </div>
-      <div className='form-group'>
-        <label for='password1'>Password</label>
-        <input
-          type='password'
-					name='password'
-          className='form-control is-invalid'
-          id='password1'
-        />
-				<div className="invalid-feedback">
-          Incorrect password or Username!
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className='form-group'>
+          <label for='username'>Username</label>
+          <input
+            type='text'
+            name='username'
+            className={`form-control ${errors.username && 'is-invalid'} ${
+              auth && 'is-invalid'
+            }`}
+            value={form.username}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            id='username'
+          />
+          {errors.username && (
+            <div className='invalid-feedback'>{errors.username}</div>
+          )}
         </div>
-      </div>
-      <button type='submit' className='btn btn-primary'>
-        Login
-      </button>
-    </form>
+        <div className='form-group'>
+          <label for='password1'>Password</label>
+          <input
+            type='password'
+            name='password'
+            className={`form-control ${errors.password && 'is-invalid'} ${
+              auth && 'is-invalid'
+            }`}
+            id='password1'
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={form.password}
+          />
+          {errors.password && (
+            <div className='invalid-feedback'>{errors.password}</div>
+          )}
+          {auth && (
+            <div className='invalid-feedback'>
+              "Username" or "Password" incorrect!!!
+            </div>
+          )}
+        </div>
+        <button type='submit' className='btn btn-primary'>
+          Login
+        </button>
+      </form>
+      {loading && <Loader />}
+    </div>
   )
 }
 
