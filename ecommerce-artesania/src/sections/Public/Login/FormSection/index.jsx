0072b9af4f9f741c771a48c9f79c22454c95daa8
torvic <130111@unsaac.edu.pe>
2021-08-29 23:53:56 -0500

    Feat(login): Add new title

--- ecommerce-artesania/src/sections/Public/Login/FormSection/index.jsx
+++ ecommerce-artesania/src/sections/Public/Login/FormSection/index.jsx
@@ -45,7 +45,7 @@ const FormSection = () => {
   } = useForm(initialForm, validationsForm)
   return (
     <div>
-			<h2>Login !!!</h2>
+			<h2>Iniciar sesión</h2>
       <form onSubmit={handleSubmit}>
         <div className='form-group'>
           <input
